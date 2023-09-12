a1cce134b4f9ba6dad21552d0a051fb845edf97a
Natalia Acuña <natelizau@gmail.com>
2023-09-11 21:39:06 -0300

    Ejercicios 1 y 2

--- assets/js/calculo.js
+++ assets/js/calculo.js
@@ -1,20 +1,75 @@
-let initialPriceElement = document.querySelector('.initialPrice'); // variables con clases en el DOM
-let quantityElement = document.querySelector('.quantity');
-let masButton = document.querySelector('.mas');
-let menosButton = document.querySelector('.menos');
-let totalSection = document.querySelector('.total-section');
-let valorTotalElement = document.querySelector('.valor-total');
-const formateador = new Intl.NumberFormat("en", { style: "currency", "currency": "CLP" });
+//Ejercicio 1
+let tieneBorde = false
 
-masButton.addEventListener('click', () => {  //al hacer click aumenta el precio 
-    quantityElement.innerHTML++;
-    valorTotalElement.innerHTML = formateador.format(quantityElement.innerHTML * initialPriceElement.innerHTML);
+let imagen = document.querySelector('#image');
+imagen.addEventListener('click', () => {
+    if (tieneBorde == false) {
+        imagen.classList.add("borde-rojo")
+        tieneBorde = true
+    } else {
+        imagen.classList.remove("borde-rojo")
+        tieneBorde = false
+    }
+
 });
 
-menosButton.addEventListener('click', () => { //al hacer click disminuye el precio 
-    quantityElement.innerHTML--;
-    valorTotalElement.innerHTML = formateador.format(quantityElement.innerHTML * initialPriceElement.innerHTML);
+
+//Ejercicio 2
+
+let sanz = document.querySelector('#sanz');
+let lolito = document.querySelector('#lolito');
+let melendi = document.querySelector('#melendi');
+let totalsection = document.querySelector('#totalsection');
+
+let valorsanz = 0;
+let valorlolito = 0;
+let valormelendi = 0;
+
+sanz.addEventListener('change', (event) => {
+    valorsanz = event.target.valueAsNumber || 0;
+    let total = valorsanz + valorlolito + valormelendi;
+    if (total <= 10) { totalsection.innerHTML = "Llevas " + total + " Stickers" }
+    else { totalsection.innerHTML = "Llevas demasiados stickers" }
+})
+lolito.addEventListener('change', (event) => {
+    valorlolito = event.target.valueAsNumber || 0;
+    let total = valorsanz + valorlolito + valormelendi;
+    if (total <= 10) { totalsection.innerHTML = "Llevas " + total + " Stickers" }
+    else { totalsection.innerHTML = "Llevas demasiados stickers" }
+})
+melendi.addEventListener('change', (event) => {
+    valormelendi = event.target.valueAsNumber || 0;
+    let total = valorsanz + valorlolito + valormelendi;
+    if (total <= 10) { totalsection.innerHTML = "Llevas " + total + " Stickers" }
+    else { totalsection.innerHTML = "Llevas demasiados stickers" }
+})
+
+
+
+//Ejercicio 3
+let boton3 = document.querySelector('#boton3');
+boton3.addEventListener('click', () => {
+    let primero = document.querySelector('#selector-1').value
+    let segundo = document.querySelector('#selector-2').value
+    let tercero = document.querySelector('#selector-3').value
+    if (primero == '9' && segundo == '1' && tercero == '1') {
+        resultado = document.querySelector('#resultado')
+        resultado.innerHTML = 'Password 1 correcto'
+    }
+    else if (primero == '7' && segundo == '1' && tercero == '4') {
+        resultado = document.querySelector('#resultado')
+        resultado.innerHTML = 'Password 2 correcto'
+    }
+    else {
+        resultado = document.querySelector('#resultado')
+        resultado.innerHTML = 'password incorrecto'
+    }
+
 });
+
+
+
+
 
 
 
